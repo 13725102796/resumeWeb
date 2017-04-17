e65c79a352548057c7c6e2db4d88aea50939b6ef
Yechang Zhang <1439655764@qq.com>
2017-04-17 22:28:49 +0800

    experience.ejs后台搭建
    完善经验栏目的功能建设

--- route/admin/experience.js
+++ route/admin/experience.js
@@ -19,7 +19,7 @@ module.exports = function () {
                     if (err) {
                         res.status(500).send('database error').end();
                     } else {
-                        res.redirect('/admin/intro')
+                        res.redirect('/admin/experience')
                     }
                 });
             }else {
@@ -27,11 +27,11 @@ module.exports = function () {
                 res.send('您暂时还没有权限删除全部数据').end();
             }
         }else{
-            db.query(`SELECT * FROM experience_table`, (err, aboutData)=>{
+            db.query(`SELECT * FROM experience_table`, (err, experienceData)=>{
                 if(err){
                     res.status(500).send('database error').end();
                 }else{
-                    res.render('page/experience.ejs', {aboutData});
+                    res.render('page/experience.ejs', {experienceData});
                 }
             });
         }
@@ -40,29 +40,31 @@ module.exports = function () {
     router.post('/', (req, res)=>{
         "use strict";
         //获取到提交的内容信息
+        var workTime = req.body.time;
         var title = req.body.title;
         var introduction = req.body.introduction;
+        var icon = req.body.icon;
         if(!title || !introduction){
             res.status(400).send('填写内容不能为空').end();
         }else{
             if(req.body.mod_id){
                 //修改逻辑
-                db.query(`UPDATE experience_table SET title='${title}', content='${introduction}' WHERE ID='${req.body.mod_id}'`, (err, data)=>{
+                db.query(`UPDATE experience_table SET worktime='${workTime}', title='${title}', content='${introduction}', icon_color='${icon}' WHERE ID='${req.body.mod_id}'`, (err, data)=>{
                     if(err){
                         console.error(err);
                         res.status(500).send('database error').end();
                     }else{
-                        res.redirect('/admin/intro');
+                        res.redirect('/admin/experience');
                     }
                 });
             }else{
                 //新添加逻辑
-                db.query(`INSERT INTO experience_table (title, content) VALUES('${title}', '${introduction}')`, (err, data)=>{
+                db.query(`INSERT INTO experience_table (worktime, title, content, icon_color) VALUES( '${workTime}','${title}', '${introduction}', '${icon}')`, (err, data)=>{
                     if(err){
                         console.error(err);
                         res.status(500).send('database error').end();
                     }else{
-                        res.redirect('/admin/intro');
+                        res.redirect('/admin/experience');
                     }
                 });
             }
